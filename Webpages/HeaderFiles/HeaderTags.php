b1e492d1cbeff61a0b8b54eee7a96a280074db41
klopez5783 <lkristopher62@gmail.com>
2024-03-20 13:57:53 -0400

    Added header.php for dynamic inclusion of CSS file (PageStyles.css) into HTML documents. This allows for easy styling across multiple pages. Additionally, included styles for background image (../../Images/Background.jpeg) covering the entire viewport and centered within the bgImage class.

---
+++ Webpages/HeaderFiles/HeaderTags.php
@@ -0,0 +1,4 @@
+<?php
+// Echo the link tag to include the CSS file
+echo '<link rel="stylesheet" type="text/css" href="HeaderFiles/PageStyles.css">';
+?>
---
+++ Webpages/Home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Web Page</title>
+    <!-- Add your CSS files here -->
+    <link rel="stylesheet" href="styles.css">
+    <!-- Add any meta tags, external stylesheets, or scripts here -->
+
+    <?php include 'HeaderFiles/HeaderTags.php'; ?>
+</head>
+
+<!-- <style>
+    bgImage {
+        background-image: url('../Images/Background.jpeg');
+          /* Full height */
+        height: 100%; 
+
+        /* Center and scale the image nicely */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style> -->
+
+<body class="bgImage">
+
+
+<h1>TEStin</h1>
+
+</body>
+</html>
+

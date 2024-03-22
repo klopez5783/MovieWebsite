9076bb013d1fa9a3b14d5b38d4890b6122868307
klopez5783 <lkristopher62@gmail.com>
2024-03-22 17:22:02 -0400

    Centered Navbar Created Login Button that utlizes bootstrap 5 modal.

--- Webpages/HeaderFiles/HeaderTags.php
+++ Webpages/HeaderFiles/HeaderTags.php
@@ -6,5 +6,7 @@ echo '<!-- Latest compiled and minified CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <!-- Latest compiled JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>'
-?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/d8ebdfa3e8.js" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+'?>
--- Webpages/Home.php
+++ Webpages/Home.php
@@ -5,13 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Web Page</title>
     <!-- Add your CSS files here -->
-    <link rel="stylesheet" href="styles.css">
     <!-- Add any meta tags, external stylesheets, or scripts here -->
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <?php include 'HeaderFiles/HeaderTags.php'; ?>
+    <?php include 'HeaderFiles/HeaderTags.php';?>
+    <!-- <link rel="stylesheet" href="/HeaderFiles/PageStyles.css"> -->
+
+    <style>
+        .loginForm{
+            border: none;
+            border-bottom: 1px solid black;
+            outline: none;
+        }
+        /* Define bigger-input class */
+        .bigger-input {
+           
+            font-size: 16px; /* Adjust the font size as needed */
+        }
+
+
+        #loginModalContent{
+            width: 350px;
+        }
+        
+        
+
+    </style>
 
 </head>
 
@@ -25,47 +46,80 @@ $movies = getMovieObjArray()
 
     <body class="bgImage">
 
-        <div class="" id="navBar">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navBar">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Pricing</a>
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </div>
-                </div>
-            </div>
-        </nav>
-        </div>
-
-        <div class="container-fluid" id="content">
-            <!-- Set up your HTML -->
-            <div class="owl-carousel">
-                <?php
-                // Loop through the array of movie objects
-                foreach ($movies as $movie) {
-                    ?>
-                    <div class="card">
-                        <img src="..." class="card-img-top" alt="Movie Poster"> <!-- Update the src attribute with the actual movie poster image -->
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $movie->movie_name; ?></h5> <!-- Display movie name -->
-                            <p class="card-text">Director: <?php echo $movie->Director; ?></p> <!-- Display director -->
-                            <p class="card-text">Actors: <?php echo $movie->actors; ?></p> <!-- Display actors -->
-                            <p class="card-text">Release Date: <?php echo $movie->release_date; ?></p> <!-- Display release date -->
-                            <p class="card-text">Genre: <?php echo $movie->genre; ?></p> <!-- Display genre -->
-                            <p class="card-text">Rating: <?php echo $movie->rating; ?></p> <!-- Display rating -->
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="mx-auto" id="navBar">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light mx-auto" id="navBar">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#">Pricing</a>
+                        </div>
+                        <div class="navbar-nav ms-auto" id="loginDiv">
+                            <button data-bs-toggle="modal" data-bs-target="#loginModal" type="button" class="btn btn-success"><i class="fa-solid fa-right-to-bracket fa-lg"></i> Login</button>
                         </div>
                     </div>
-                <?php
-                }
-                ?>
+                    
+                </div>
+            </nav>
+        </div>
+
+
+        <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content" id="loginModalContent">
+                    <div class="modal-header text-center">
+                        <h4 class="modal-title w-100 font-weight-bold"><i class="fa-solid fa-user "></i> Login</h4>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body text-center">
+                        <div class="m-5">
+                            <i class="fa-solid fa-envelope fa-xl"></i>
+                            <input type="text" id="LoginEmail" class="loginForm bigger-input" name="LoginEmail" placeholder="Email">
+                        </div>
+                        <div class="m-5">
+                            <i class="fa-solid fa-lock fa-xl"></i>
+                            <input type="text" id="LoginPassword" class="loginForm bigger-input" name="LoginPassword" placeholder="Password">
+                            <div class="d-flex justify-content-end">
+                                <a href="" class="mt-2">Forgot Password?</a>
+                            </div>
+                        </div>
+                        
+
+                    </div>
+                    <div class="modal-footer d-flex justify-content-center">
+                        <button type="button" class="btn buttonColor">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <div class="container-fluid" id="content">
+                <!-- Set up your HTML -->
+                <div class="owl-carousel">
+                    <?php
+                    // Loop through the array of movie objects
+                    foreach ($movies as $movie) {
+                        ?>
+                        <div class="card">
+                            <img src="..." class="card-img-top" alt="Movie Poster"> <!-- Update the src attribute with the actual movie poster image -->
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $movie->movie_name; ?></h5> <!-- Display movie name -->
+                                <p class="card-text">Director: <?php echo $movie->Director; ?></p> <!-- Display director -->
+                                <p class="card-text">Actors: <?php echo $movie->actors; ?></p> <!-- Display actors -->
+                                <p class="card-text">Release Date: <?php echo $movie->release_date; ?></p> <!-- Display release date -->
+                                <p class="card-text">Genre: <?php echo $movie->genre; ?></p> <!-- Display genre -->
+                                <p class="card-text">Rating: <?php echo $movie->rating; ?></p> <!-- Display rating -->
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    <?php
+                    }
+                    ?>
             </div>
 
 
@@ -75,8 +129,8 @@ $movies = getMovieObjArray()
             $theatres = getTheatreObjArray()
             ?>
 
-            <div class="d-flex justify-content-evenly">
-                <div class="overflow-auto p-3 bg-light" style="max-width: 90%;"> 
+            <div class="d-flex justify-content-between">
+                <div class="overflow-auto p-3 m-3 bg-light " style="max-width: 400px;max-height:250px;"> 
                     <div class="list-group">
                         <?php foreach ($theatres as $theatre): ?>
                             <a href="#" class="list-group-item list-group-item-action">
@@ -86,14 +140,13 @@ $movies = getMovieObjArray()
                     </div>
                 </div> 
                 <div>
-                    <img src="../Images/map.jpg" alt="map" width="90%" height="90%">
+                    placeholder
                 </div>
             </div>
 
             
 
         </div>
-
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
